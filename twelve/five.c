139c38c5e310c3ae57024eacec589b11500ec63e
龚康佳 <gongkj@eastcom-sw.com>
2022-12-12 00:24:38 +0800

    quick-sort

--- twelve/five.c
+++ twelve/five.c
@@ -6,11 +6,14 @@
 void quickSort(int* arr, int start, int end);
 
 int main() {
-  int nums[SIZE];
+  int nums[SIZE] = { 8,1,9,9,7,7,4,8,8,1 };
   time_t seconds = time(NULL);
-  srand(seconds / 3600);
+  srand(seconds % 3600);
+  // for (int i = 0; i < SIZE; i++) {
+  //   nums[i] = rand() % 10;
+  //   printf("%d ", nums[i]);
+  // }
   for (int i = 0; i < SIZE; i++) {
-    nums[i] = rand() % 10;
     printf("%d ", nums[i]);
   }
   printf("--------------\n");
@@ -22,48 +25,32 @@ int main() {
 }
 
 void quickSort(int* arr, int start, int end) {
-  int len = end - start + 1;
-  int pivot = rand() % SIZE;
+  if (start >= end)
+    return;
+  int pivot = start + (end - start) / 2;
   int left = start;
   int right = end;
   int temp = arr[pivot];
-  if (left >= right)
-    return;
   while (left < right) {
-    while (arr[left] <= temp && left < right)
+    while (arr[left] < temp && left < right) {
       left++;
-    if (left < right) {
-      arr[pivot] = arr[left];
-      pivot = left;
     }
-
-
-    printf("%d %d %d | %d %d %d --->", left, right, pivot, arr[left], arr[right], arr[pivot]);
-    for (int i = 0; i < len; i++) {
-      printf("%d ", arr[i]);
-    }
-    printf("\n");
-    while (temp <= arr[right] && left < right)
+    arr[pivot] = arr[left];
+    pivot = left;
+    while (temp <= arr[right] && left < right) {
       right--;
-    if (left < right) {
-      arr[pivot] = arr[right];
-      pivot = right;
     }
-
-
-    printf("%d %d %d | %d %d %d --->", left, right, pivot, arr[left], arr[right], arr[pivot]);
-    for (int i = 0; i < len; i++) {
-      printf("%d ", arr[i]);
-    }
-    printf("\n");
+    arr[pivot] = right;
+    pivot = right;
   }
   arr[pivot] = temp;
-  printf("%d %d %d | %d %d %d --->", left, right, pivot, arr[left], arr[right], arr[pivot]);
-  for (int i = 0; i < len; i++) {
+
+  printf("%d %d %d | %d %d %d --->", start, end, pivot, arr[left], arr[right], arr[pivot]);
+  for (int i = 0; i < SIZE; i++) {
     printf("%d ", arr[i]);
   }
   printf("\n");
 
-  quickSort(arr, start, pivot);
-  quickSort(arr, pivot, end);
+  quickSort(arr, start, left - 1);
+  quickSort(arr, left + 1, end);
 }
